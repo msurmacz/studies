cebfecf5d634cc228e129f5c494b87b67586341b
michal.pawel.surmacz <michal.pawel.surmacz@ab112e7e-fa4a-0410-ba12-79a75b92dc02>
2008-04-23 13:04:05 +0000

    Done reading poset file type into list of lists. Next: create poset/graph from that list

--- Grafy i formy/src/gif/FormReader.java
+++ Grafy i formy/src/gif/FormReader.java
@@ -53,23 +53,18 @@ public class FormReader {
 		
 		try {
 			s = new Scanner(new BufferedReader(new FileReader(f)));
-			//http://java.sun.com/javase/6/docs/api/java/util/regex/Pattern.html#split(java.lang.CharSequence)
-			//http://www.unitedti.org/lofiversion/index.php/t6285.html
-			s.useDelimiter(java.util.regex.Pattern.compile("\\|"));
+			
 			while (s.hasNextLine()) {
 				Scanner s2 = new Scanner(s.nextLine());
-				if (!s2.hasNext()) {	//pusta linia
+				s2.useDelimiter("[\\|,]");	// separatory | albo ,
+				if (!s2.hasNext()) {	//pusta linia oznacza koniec danych
 					s2.close();
 					break;
 				}
-				while(s2.hasNext()) {	//Wczytywanie
-					System.out.println(s2.next());
-//					subList = new ArrayList<Integer>();
-//					int[] a = new int[3];
-//					for (int i = 0; i < a.length; i++)
-//						a[i] = s2.nextInt();
-//					list.add(a);
-				}
+				subList = new ArrayList<Integer>();	//wczytuje liste
+				while(s2.hasNext())
+					subList.add(s2.nextInt());
+				list.add(subList);
 			}
 			
 			poset = new Poset(list);
