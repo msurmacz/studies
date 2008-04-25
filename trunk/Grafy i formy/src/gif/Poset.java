186708c0f398aec26b041a67551702666856ee23
michal.pawel.surmacz <michal.pawel.surmacz@ab112e7e-fa4a-0410-ba12-79a75b92dc02>
2008-04-25 12:12:32 +0000

    Creating poset from list of lists

--- trunk/Grafy i formy/src/gif/Poset.java
+++ trunk/Grafy i formy/src/gif/Poset.java
@@ -2,14 +2,26 @@ package gif;
 
 import org.jgrapht.graph.*;
 import org.jgrapht.*;
+
+import java.util.Iterator;
 import java.util.List;
 
 public class Poset {
-	private DirectedGraph<Integer, DefaultEdge> graph = 
-		new SimpleDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+	private DirectedGraph<Integer, DefaultEdge> graph;
 	
 	public Poset(List<List<Integer>> list) {
-		
+		graph = new SimpleDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
+
+		for (List<Integer> subList : list) {
+			Iterator<Integer> it = subList.iterator();
+			int source = it.next();
+			graph.addVertex(source);
+			while (it.hasNext()) {
+				int v = it.next();
+				graph.addVertex(v);
+				graph.addEdge(source, v);
+			}
+		}
 	}
 
 }
