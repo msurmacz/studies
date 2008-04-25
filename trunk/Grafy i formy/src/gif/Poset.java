13889ea7b7697468e5bc8ab9ef3567031b16bfb2
michal.pawel.surmacz <michal.pawel.surmacz@ab112e7e-fa4a-0410-ba12-79a75b92dc02>
2008-04-25 16:32:14 +0000

    Constructor Form(Poset) finished. Which finishes whole project :) ver 1.0

--- trunk/Grafy i formy/src/gif/Poset.java
+++ trunk/Grafy i formy/src/gif/Poset.java
@@ -3,8 +3,13 @@ package gif;
 import org.jgrapht.graph.*;
 import org.jgrapht.*;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Poset {
 	private DirectedGraph<Integer, DefaultEdge> graph;
@@ -23,5 +28,58 @@ public class Poset {
 			}
 		}
 	}
+	
+	public Set<Integer> getVertices() {
+		return graph.vertexSet();
+	}
+	
+	public Set<Integer> getMaxI() {
+		Set<Integer> maxI = new TreeSet<Integer>();
+		
+		for (int v : graph.vertexSet())
+			if (graph.outDegreeOf(v) == 0)
+				maxI.add(v);
+		
+		return maxI;
+	}
 
+	public Set<Integer> getIMinus() {
+		Set<Integer> iMinus = new TreeSet<Integer>(graph.vertexSet());
+		iMinus.removeAll(getMaxI());
+		
+		return iMinus;
+	}
+	
+	/**
+	 * Returns set of vertices that are "less then" root (in terms of poset).
+	 * <p>
+	 * This is kind of Breadth-First Traversal.
+	 * 
+	 * @param root	where the searching starts
+	 * @return	set of vertices less then root
+	 */
+	public Set<Integer> lessThan(int root) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		Set<Integer> visited = new HashSet<Integer>();
+		
+		q.add(root);
+		visited.add(root);
+		
+		while (!q.isEmpty()) {
+			Set<DefaultEdge> edges;
+			int v = q.remove();
+			visited.add(v);
+			edges = graph.incomingEdgesOf(v);
+			for (DefaultEdge e : edges) {
+				int source = graph.getEdgeSource(e);
+				if (!visited.contains(source)) {
+					q.add(source);
+					visited.add(source);
+				}
+			}
+		}
+		visited.remove(root);
+		
+		return new TreeSet<Integer>(visited);
+	}
 }
